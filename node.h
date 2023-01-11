475b90be37146b40be840a9dd74de697e2c89f14
minecraftpr03 <47282021+MasterCoder21@users.noreply.github.com>
2023-01-10 20:52:02 -0800

    Update node.h

--- node.h
+++ node.h
@@ -3,17 +3,23 @@
 
 #include "student.h"
 
-class Node {
-    public:
-        Node(Student* student);
-        ~Node();
-        Node* getNext();
-        void setNext(Node* next);
-        Student* getStudent();
-        void setStudent(Student* student);
-    private:
-        Node* next;
-        Student* student;
+class Node
+{
+public:
+    Node(Student *student);
+    ~Node();
+    Node *getNext();
+    void setNext(Node *next);
+    Student *getStudent();
+
+private:
+    Node *next;
+    Student *student;
 };
 
 #endif // NODE_H
+/*
+This is the Node class!  
+It represents a Node in the linked list.  
+It contains a Student pointer and another Node pointer, which points to the next Node in the series.
+*/
